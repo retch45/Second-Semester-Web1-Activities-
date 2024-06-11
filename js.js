fd7e74c98e16b78664b2087790d29a43de70ae5f
retch45 <rechellecarpio4@gmail.com>
2024-06-11 16:07:41 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,41 @@
+function put1  (){
+    document.getElementById("lab1").style.color="red";
+    document.getElementById("lab2").style.color="green";
+}
+function put2  (){
+    document.getElementById("lab1").style.color="green";
+    document.getElementById("lab2").style.color="red";
+}
+function put3  (){
+    document.getElementById("lab1").style.color="green";
+    document.getElementById("lab2").style.color="red";
+}
+function put4  (){
+    document.getElementById("lab1").style.color="red";
+    document.getElementById("lab2").style.color="green";
+}
+function put5 (){
+    document.getElementById("lab1").style.color="red";
+    document.getElementById("lab2").style.color="green";
+}
+function put6 (){
+    document.getElementById("lab1").style.color="red";
+    document.getElementById("lab2").style.color="green";
+  
+}
+function put7 (){
+    document.getElementById("lab1").style.color="green";
+    document.getElementById("lab2").style.color="red";
+}
+function put8  (){
+    document.getElementById("lab1").style.color="red";
+    document.getElementById("lab2").style.color="green";
+}
+function put9  (){
+    document.getElementById("lab1").style.color="red";
+    document.getElementById("lab2").style.color="green";
+}
+function put10  (){
+    document.getElementById("lab1").style.color="red";
+    document.getElementById("lab2").style.color="green";
+}
